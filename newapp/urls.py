84495e4bead938fc4ab4c5c43947cc20bd7fcb49
Razaklekan113 <razaklekan11@gmail.com>
2023-05-07 21:52:27 +0100

    commited on this files

--- newapp/urls.py
+++ newapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.homepage),
     path("addnew/", views.addnew),
+    path("test/", views.test),
 ]
--- newapp/views.py
+++ newapp/views.py
@@ -3,16 +3,19 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Customer
 from .forms import CustomerForm
 
+
+def test(request):
+    html = "<html><body><h1>Hello World</h1></body></html"
+    return HttpResponse(html)
+
+
 def addnew(request):
-
     if request.method == 'POST':
-
         formset = CustomerForm(request.POST)
 
         if formset.is_valid():
             formset.save()
             return HttpResponseRedirect("/")
-
     else:
         formset = CustomerForm()
         return render(request, 'addnew.html', {'formset': formset})
@@ -24,8 +27,5 @@ def homepage(request):
     Returns index.html template - db data returned
 
     """
-
     x = Customer.objects.all()
-
-
     return render(request, 'index.html', {'data': x})
